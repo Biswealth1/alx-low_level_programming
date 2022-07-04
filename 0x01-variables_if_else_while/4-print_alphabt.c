7a1b6fdcc26b5a72d0fca4ffb77d9508214c90d2
Biswealth1 <abisolaa282@gmail.com>
2022-07-04 08:21:47 -0700

    tashj

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   * Return: Always 0 (success)
- *
- *    */
-
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
-
 {
-
-		char ch;
-
-
-
-			for (ch = 'a'; ch <= 'z'; ch++)
-
-						if (ch != 'e' && ch != 'q')
-
-										putchar(ch);
-
-				putchar('\n');
-
-					return (0);
-
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+putchar('\n');
+return (0);
 }
