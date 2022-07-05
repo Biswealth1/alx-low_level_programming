f60c78bceb6db9c84b774c932467721e9cfaf12f
Biswealth1 <abisolaa282@gmail.com>
2022-07-04 17:35:12 -0700

    task99

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,45 +1,20 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   * Return: Always 0 (success)
- *
- *    */
-
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
-
 {
-
-		int a;
-
-
-
-			for (a = 0; a < 10; a++)
-
-					{
-
-								putchar((a % 10) + '0');
-
-										if (a == 9)
-
-														continue;
-
-
-
-												putchar(',');
-
-														putchar(' ');
-
-															}
-
-				putchar('\n');
-
-					return (0);
-
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar((a % 10) + '0');
+if (a == 9)									continue;
+putchar(',');									putchar(' ');
+}
+putchar('\n');
+return (0);
 }
